a09f0f365360571a2305980329206bead9f50133
SnowSword1010 <jmayank2001@gmail.com>
2021-07-08 21:35:40 +0530

    :sparkles: Configure lines Buttons added

--- main.py
+++ main.py
@@ -20,17 +20,51 @@ logo = ImageTk.PhotoImage(logo)
 logo_label = tk.Label(image=logo)
 # critical line ; can't be skipped
 logo_label.image = logo
-logo_label.grid(column=0, row=0)
+logo_label.grid(column=0, row=0, columnspan=3)
 
 # SUBTITLE WIDGET
 subtitleFont = tkinter.font.Font(family = "Raleway", size = 12, slant="italic", weight="bold")
 subtitleLabel = tk.Label(root, text = "Automation Software", font = subtitleFont, justify="center")
-subtitleLabel.grid(row=1, column = 0, pady = 5)
+subtitleLabel.grid(row=1, column = 0, pady = 5, columnspan=3)
 
 # INSTRUCTIONS WIDGET
 instructionsFont = tkinter.font.Font(family = "Raleway", size = 12)
 instructionsText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 instructionsLabel = tk.Label(root, text = instructionsText, font = instructionsFont, justify="center", wraplength=700)
-instructionsLabel.grid(row=2, column=0, pady= (20,0))
+instructionsLabel.grid(row=2, column=0, pady= (20,15), columnspan=3)
+
+# CONFIGURE LINES WIDGET
+# HeadingFont = tkinter.font.Font(family = "Raleway", size = 12, weight="bold")
+# HeadingText = "Configure Lines"
+# HeadingLabel = tk.Label(root, text = HeadingText, font=HeadingFont, justify="left")
+# HeadingLabel.grid(row=3, column=0, sticky='W', columnspan = 3)
+
+
+# ADD, REMOVE, UPDATE BUTTON FUNCTIONS
+
+def add_line_clicked():
+    print("Add line clicked")
+    return
+
+def update_line_clicked():
+    print("Update line clicked")
+    return
+
+def remove_line_clicked():
+    print("Remove line clicked")
+    return
+
+# ADD, REMOVE, UPDATE BUTTON FRAME
+frame = tk.LabelFrame(root, text="Configure Lines", pady=10)
+frame.grid(row=3, column=0, columnspan=3, sticky='W')
+
+# ADD, REMOVE, UPDATE BUTTON WIDGETS
+add_button = tk.Button(frame, text="Add Line", command=lambda: add_line_clicked(), padx=10, pady=5)
+update_button = tk.Button(frame, text="Update Line", command=lambda: update_line_clicked(), padx=10, pady=5)
+remove_button = tk.Button(frame, text="Remove Line", command=lambda: remove_line_clicked(), padx=10, pady=5)
+
+add_button.grid(row=0, column=0, padx=(81, 81))
+update_button.grid(row=0, column=1, padx=(81, 81))
+remove_button.grid(row=0, column=2, padx=(81 , 81))
 
 root.mainloop()
